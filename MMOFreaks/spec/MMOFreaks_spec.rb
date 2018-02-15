9b2f82892b125a4423c1391ebccd0ae99456f3b7
catalanowebdevelopment <catalano.work@gmail.com>
2018-02-15 12:18:11 -0500

    Created the initial setup for MMOFreaks

---
+++ MMOFreaks/spec/MMOFreaks_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MMOFreaks do
+  it "has a version number" do
+    expect(MMOFreaks::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
