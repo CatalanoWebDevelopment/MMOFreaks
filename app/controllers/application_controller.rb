d527fa8f28e53c104e0c5184b1fb4129279248c4
catalanowebdevelopment <catalano.work@gmail.com>
2018-09-10 12:10:03 -0400

    Changed configuration to :public_folder , 'app/public' in Application Controller for CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder , 'public'
+    set :public_folder , 'app/public'
     set :views, 'app/views'
     enable :sessions
     register Sinatra::Flash
